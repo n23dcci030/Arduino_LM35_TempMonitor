f418e17dd97af18f1b992a3187f3bd1b00ae8b89
n23dcci030 <n23dcci030@student.ptithcm.edu.vn>
2026-04-15 21:37:02 +0700

    feat: doi dinh dang serial sang JSON

--- Arduino_LM35_TempMonitor.ino
+++ Arduino_LM35_TempMonitor.ino
@@ -3,14 +3,11 @@ void setup() {
 }
 
 void loop() {
-  int valueA0 = analogRead(A0);
-  int valueA1 = analogRead(A1);
+  int temp = analogRead(A0);
   int valueA2 = analogRead(A2);
 
   Serial.print("{\"A0\": ");
-  Serial.print(valueA0);
-  Serial.print(", \"A1\": ");
-  Serial.print(valueA1);
+  Serial.print(temp);
   Serial.print(", \"A2\": ");
   Serial.print(valueA2);
   Serial.println("}");
