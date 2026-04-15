1b5879072d96a16a3935f4caf2bdf6cda97d33cb
n23dcci030 <n23dcci030@student.ptithcm.edu.vn>
2026-04-15 14:25:31 +0700

    feat: them kenh A2

--- Arduino_LM35_TempMonitor.ino
+++ Arduino_LM35_TempMonitor.ino
@@ -3,7 +3,14 @@ void setup() {
 }
 
 void loop() {
+  int temp = analogRead(A0);
   int valueA2 = analogRead(A2);
+
+  Serial.print("A0: ");
+  Serial.println(temp);
+
+  Serial.print("A2: ");
   Serial.println(valueA2);
+
   delay(1000);
 }
