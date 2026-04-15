b4d8de886aef6f93b3afe572543cf7a0f0e80199
Trung <trungnguyen.8102005@gmail.com>
2026-04-15 10:07:15 +0700

    SV B: Doi sang dinh dang JSON

--- Arduino_LM35_TempMonitor/LM35_TempReader/LM35_TempReader.ino
+++ Arduino_LM35_TempMonitor/LM35_TempReader/LM35_TempReader.ino
@@ -12,9 +12,12 @@ void loop() {
   float temp1 = (value1 * 500.0) / 1023.0;
   float temp2 = (value2 * 500.0) / 1023.0;
 
+// Thay thế 3 dòng cũ bằng dòng này để ra định dạng JSON
+  Serial.print("{\"temp1\":");
   Serial.print(temp1);
-  Serial.print(",");
-  Serial.println("{\"status\": \"JSON format\"}");
+  Serial.print(", \"temp2\":");
+  Serial.print(temp2);
+  Serial.println("}");
   
 
   delay(1000);
